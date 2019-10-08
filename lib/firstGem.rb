601c3f7ec302d550642631d832d05163acb103e6
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-10-08 12:15:10 -0400

    tinky winky

--- lib/firstGem.rb
+++ lib/firstGem.rb
@@ -105,6 +105,73 @@ odddhhyyyyyy/`   ..-/yhyy:..````.:++o+.``````.-://--::-....:sss``.---`
 
 
       def tinkyWinky
+        puts "
+        
+          `-/os-                                                 
+        ./shddddo                                                 
+     .+yddy+-.hd/                                                 
+   -+ydhs:.   ody`                                                 
+`/ydho:`     :dd-                                                  
+sddo.       `hd+                                                   
+-oyhhyso+/::ody`                      `/shhs+.                     
+  `.-/+syhddd/                       oddddddh:                    
+        ``-sdy//++++/:-`           ``hddddddds                    
+         `:shhhhyyyyhhhhs/`    -/.:hhdddddddds                    
+       `+yhyo/-------/oyddy:  :hso..+hddddddd/                    
+      .shs/....-------::+yddo-hdyy+` .odddddds`                   
+     `yh/.....--------:://ohddmdhhs-  /ddddddd/                   
+     +h/......------:://///oddddhhy:  sdddddddy                   
+     oy......-----::+oooo+//sdddhhy- `hdddddddd                   
+     +y....--------/oysmdo//ohddhho` .hdddddddd                   
+ -:-.:h:..:+/++-..--:+++/:/+oyddyo.  .ddddddddd                   
+ :oyhhhs.-:/ssho.--:::::::/+oyho+.  -oddddddddd                   
+  `:+yhho--:+o+-..---:/+o//+oh:  `:shdddddddddh                   
+   `.-+yhs:---...-:+syys+++sy: .+ydddddddddddd+                   
+     `.-/sy+::--:/+oo+/+sydd//shdddddddddddddo`                   
+       `.-oyo+/////+osyhdddddddddddddddddddh/`                    
+         ``.:yhhhhddddddddddddddddddddddddy-                      
+          `/hddddddddddddddddddddddddddddo`                       
+         `ohdddddddddddddddddddddddddddh/`                        
+         ohdddddddddddddddddddddddddddd/                          
+        /hddddddddddddddddddddddddddddy`                          
+       -hhddhhddddddddddddddddddddddddy`                          
+       oddddhhhdddddddddddddddddddddddh`                          
+      .hddddhhhhddddddddddddddddddddddh-                          
+      :hddddhhhdddddddddddddddddddddddd+                          
+      ohddddhhdhhhhhhhhhhhhhyyyyyyhdddhy`                         
+     `hdddddhso+++++++++++++++++++odddhh/                         
+     -dddddhh//////////+++++++++++ohddddy`                        
+     +dddddhh//////////++++++++++++yddddh:                        
+     sddddhhh//////////++++++++++++oddddds`                       
+    `hddddhhh//////////++++++++++++ohddddh-                       
+    -dddddhhh//////////++++++++++o++hdddddo                       
+    -dddddhhh+//////////++++++++++++ydddddy                       
+    `yddddhhhs//////////++++++++++++ydddddh`                      
+     :hh+shhhho+++/+++++++++oooooooshdddddy                       
+      .` .hhhhhhhhhhhhhhhhdddddddddddddddds                       
+         `yhhhhhdddddddddddddddddddddddddh+                       
+          +hhhhhdddddddddddddddddddddddddh-                       
+          .yhhhhddddddddddddddddddddddddhy`                       
+           +hddddddddddddmddddddddddddddh:                        
+           `yhdddddddddddmdddddddddddddds`                        
+            /hhddddddddddmmddddddddddddh-                         
+            `ohhhdddddddddmdddddddddddd+                          
+             `shhhhdddddddmdddddddddddy`                          
+              .yhhhdddddddmddddddddddh-                           
+               .yhhdddddddmddddddddddo                            
+                -yhhddddddmdddddddddh.                            
+                 -hhhdddddmhdddddddh/                             
+                  /hddddddd/ddddddds                              
+                 .yddddddmmsdddddddh.                             
+                -ydddddddmmddddddddds                             
+               .hddddddddmmdddddddddd/                            
+               yhddddddddmdddddddddddy`                           
+              `hhdddddddddddddddddddds                            
+               -shhddhyo:`/hdddddddds`                            
+                  ```      `/osyss+-                   
+
+        "
+
       end
 
 
@@ -129,6 +196,11 @@ odddhhyyyyyy/`   ..-/yhyy:..````.:++o+.``````.-://--::-....:sss``.---`
                 ┈╱▔┈┈▕┈┈┈┈▏┈┈▔╲▔▔
                 ┈╲▂▂▂╱┈┈┈┈╲▂▂▂╱┈"
       end
+
+
+
+
+
 
     end
   end
